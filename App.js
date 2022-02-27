91da16a4e65fdb2edd033d7182de72c1b6c75f5f
Vietfu Tang <29468130+vietfu@users.noreply.github.com>
2022-02-27 14:20:32 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./screens/Home";
+import SettingsScreen from "./screens/Settings";
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{ tabBarIcon: makeIconRender("home") }}
+            />
+            <Tab.Screen
+                name="Settings"
+                component={SettingsScreen}
+                options={{ tabBarIcon: makeIconRender("cog") }}
+            />
+        </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function makeIconRender(name) {
+    return ({ color, size }) => (
+        <MaterialCommunityIcons name={name} color={color} size={size} />
+    );
+}
