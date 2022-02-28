5f7a353bccebe49a1a949a62f53e77c15eb4094f
WDaven <71561085+WDaven@users.noreply.github.com>
2022-02-28 16:03:09 -0500

    Rename Files

--- screens/HomeList.js
+++ screens/HomeList.js
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function HomeScreen() {
+export default function HomeListScreen() {
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
             <Text>HomeList!</Text>
--- screens/HomeMap.js
+++ screens/HomeMap.js
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function HomeScreen() {
+export default function HomeMapScreen() {
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
             <Text>HomeMap!</Text>
