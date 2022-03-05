81038406b2776d9e574bc5f94fe1f6ccf0a6ccbc
unknown <akandimalla6@gatech.edu>
2022-03-04 19:19:23 -0500

    Integrated FilterScreen

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./screens/Home";
 import SettingsScreen from "./screens/Settings";
+import FilterScreen from "./screens/FilterScreen";
+import { NativeBaseProvider, Text, Box } from 'native-base';
+
 
 const Tab = createBottomTabNavigator();
 
@@ -18,6 +21,11 @@ export default function App() {
             <Tab.Screen
                 name="Settings"
                 component={SettingsScreen}
+                options={{ tabBarIcon: makeIconRender("cog") }}
+            />
+            <Tab.Screen
+                name="FilterScreen"
+                component={FilterScreen}
                 options={{ tabBarIcon: makeIconRender("cog") }}
             />
         </Tab.Navigator>
