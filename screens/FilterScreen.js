c93c6242b116ca02ae55d7ab7ef35fa8656c16ef
unknown <akandimalla6@gatech.edu>
2022-03-05 15:27:48 -0500

    add checkboxes

--- screens/FilterScreen.js
+++ screens/FilterScreen.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { Button, Modal, VStack, HStack, Text, Radio, Center, NativeBaseProvider } from "native-base";
+import { Button, Modal, VStack, HStack, Text, Checkbox, Center, NativeBaseProvider } from "native-base";
 import { useState } from "react";
 
 const Example = () => {
   const [showModal, setShowModal] = useState(false);
+  const [locationValues, setLocationValues] = React.useState([]);
+  const [roommateValues, setRoommateValues] = React.useState([]);
+  const [bathroomValues, setBathroomValues] = React.useState([]);
   return <Center>
       <Button onPress={() => setShowModal(true)}>Button</Button>
       <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
@@ -19,6 +22,14 @@ const Example = () => {
                 <Text color="blueGray.400">Select 1 or more</Text>
               </HStack>
               <HStack alignItems="center" justifyContent="space-between">
+              <Checkbox.Group onChange={setLocationValues} value={locationValues} accessibilityLabel="choose numbers">
+              <Checkbox value="one" my={2}>
+                East
+              </Checkbox>
+                <Checkbox value="two">West</Checkbox>
+              </Checkbox.Group>
+              </HStack>
+              <HStack alignItems="center" justifyContent="space-between">
                 <Text fontWeight="medium">Reviews</Text>
               </HStack>
               <HStack alignItems="center" justifyContent="space-between">
@@ -32,10 +43,35 @@ const Example = () => {
                 <Text color="blueGray.400">Select 1 or more</Text>
               </HStack>
               <HStack alignItems="center" justifyContent="space-between">
+              <Checkbox.Group onChange={setRoommateValues} value={roommateValues} accessibilityLabel="choose numbers">
+              <Checkbox value="one" my={2}>
+                1
+              </Checkbox>
+                <Checkbox value="two">2</Checkbox>
+                <Checkbox value="one" my={2}>
+                3
+              </Checkbox>
+                <Checkbox value="two">4+</Checkbox>
+              </Checkbox.Group>
+              
+              </HStack>
+              <HStack alignItems="center" justifyContent="space-between">
                 <Text fontWeight="medium">Bathroom</Text>
               </HStack>
               <HStack alignItems="center" justifyContent="space-between">
                 <Text color="blueGray.400">Select 1 or more</Text>
+              </HStack>
+              <HStack alignItems="center" justifyContent="space-between">
+              <Checkbox.Group onChange={setBathroomValues} value={bathroomValues} accessibilityLabel="choose numbers">
+              <Checkbox value="one" my={2}>
+                Single
+              </Checkbox>
+                <Checkbox value="two">Shared - 1 other person</Checkbox>
+                <Checkbox value="one" my={2}>
+                Shared floor - same gender
+              </Checkbox>
+              </Checkbox.Group>
+              
               </HStack>
               <HStack alignItems="center" justifyContent="space-between">
                 <Text fontWeight="medium">Budget</Text>
