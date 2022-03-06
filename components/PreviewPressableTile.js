6e06b3bdb77267c7e013df570cd772581e862982
WDaven <71561085+WDaven@users.noreply.github.com>
2022-03-06 18:12:41 -0500

    basic tile pressable

--- components/PreviewPressableTile.js
+++ components/PreviewPressableTile.js
@@ -1,47 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text,Pressable, Image } from 'react-native';
+import React from "react";
+import {Heading, AspectRatio, Pressable, Text, Box, HStack, Spacer, Flex, Badge, Center, NativeBaseProvider, Image } from "native-base";
+//cloudman dorm image
+//https://housing.gatech.edu/sites/default/files/styles/juicebox_medium/public/building/gallery/cld-bldg.jpg?itok=WmoIH4Ag
+<Image source= {{uri :'https://housing.gatech.edu/sites/default/files/styles/juicebox_medium/public/building/gallery/cld-bldg.jpg'}}/>
 
-// Needs large overhaul this is just a starting point from an existing file I had
+function Example() {
+  return <Box alignItems="center">
+      <Pressable onPress={() => console.log("I'm Pressed")}>
+        <Box maxW="96" borderWidth="1" borderColor="coolGray.300" shadow="3" bg="coolGray.100" p="5" rounded="8" borderHeight="100">
+        <Box>
+          <AspectRatio w="100%" ratio={25 / 9}>
+            <Image source={{
+            uri: "https://housing.gatech.edu/sites/default/files/styles/juicebox_medium/public/building/gallery/cld-bldg.jpg?itok=WmoIH4Ag"
+          }} alt="image" />
+          </AspectRatio>
+          <Center bg="darkBlue.700" _dark={{
+          bg: "violet.700"
+        }} _text={{
+          color: "warmGray.50",
+          fontWeight: "700",
+          fontSize: "xs"
+        }} position="absolute" bottom="0" px="3" py="1.5">
+            Cloudman
+          </Center>
+        </Box>
+          <HStack alignItems="center">
+            <Badge colorScheme="darkBlue" _text={{
+            color: "white"
+          }} variant="solid" rounded="4">
+              Cloudman
+            </Badge>
 
-
-export default function FoodButton(props) {
-    const { onPress, btnText, resturantImage } = props;
-    return (
-      <Pressable style={styles.button} onPress={onPress}>
-        <Text style={styles.text}>{btnText}</Text>
-        <Image style={styles.image} source={resturantImage}/>
+            <Badge colorScheme="lightGreen" _text={{
+            color: "white"
+          }} variant="solid" rounded="4">
+              Bike Storage
+            </Badge>
+            <Spacer />
+            <Text fontSize={10} color="coolGray.800">
+              4.8⭐
+            </Text>
+          </HStack>
+        </Box>
       </Pressable>
-    );
-  }
-
-const styles = StyleSheet.create({
-    button: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingHorizontal: 12,
-      borderRadius: 4,
-      elevation: 3,
-      backgroundColor: 'white',
-      borderColor: 'black',
-    },
-    text: {
-      alignSelf:'flex-end',
-      fontSize: 30,
-      lineHeight: 30,
-      fontWeight: 'bold',
-      letterSpacing: 0.25,
-      color: 'black',
-      top: '50%',
-      
-    },
-    image: {
-      alignSelf:'flex-start',
-      width: 150,
-      height: 60,
-      resizeMode: 'contain',
-      marginBottom: 20,
+    </Box>;
+}
 
 
-
-  }
-  });
+export default () => {
+  return (
+    <NativeBaseProvider>
+      <Center flex={1} px="3">
+          <Example />
+      </Center>
+    </NativeBaseProvider>
+  );
+};
--- screens/HomeList.js
+++ screens/HomeList.js
@@ -1,9 +1,23 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
+import PreviewPressableTile from "../components/PreviewPressableTile";
 
 export default function HomeListScreen() {
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>HomeList!</Text>
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center", paddingTop:50 }}>
+            <ScrollView>
+            <PreviewPressableTile></PreviewPressableTile>
+            <PreviewPressableTile></PreviewPressableTile>
+            <PreviewPressableTile></PreviewPressableTile>
+            <PreviewPressableTile></PreviewPressableTile>
+            <PreviewPressableTile></PreviewPressableTile>
+            <PreviewPressableTile></PreviewPressableTile>
+            <PreviewPressableTile></PreviewPressableTile>
+            <PreviewPressableTile></PreviewPressableTile>
+            <PreviewPressableTile></PreviewPressableTile>
+            <PreviewPressableTile></PreviewPressableTile>
+            <PreviewPressableTile></PreviewPressableTile>
+            <PreviewPressableTile></PreviewPressableTile>
+            </ScrollView>
         </View>
     );
 }
