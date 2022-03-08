e935558a6d474ee3517341ee6a66953f556e0ac1
WDaven <71561085+WDaven@users.noreply.github.com>
2022-03-07 22:28:56 -0500

    styling work on fitler component

--- components/FilterComponent.js
+++ components/FilterComponent.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Box, Modal, Stack, VStack, HStack, Text, Checkbox, Center, Slider, NativeBaseProvider, View, Slide, Button} from "native-base";
+import { Box, Image, Modal, Stack, VStack, HStack, Text, Checkbox, Center, Slider, NativeBaseProvider, View, Slide, Button, Icon} from "native-base";
 import { useState } from "react";
 import MultiSlider from '@ptomasroos/react-native-multi-slider';
 import { Rating, AirbnbRating } from 'react-native-ratings';
+import { MaterialCommunityIcons } from "@expo/vector-icons"; 
 const Example = () => {
   const [showModal, setShowModal] = useState(false);
   const [locationValues, setLocationValues] = React.useState([]);
@@ -22,99 +23,125 @@ const Example = () => {
           <Modal.CloseButton />
           <Modal.Header>Filters</Modal.Header>
           <Modal.Body>
-          <VStack space={3}>
-              <HStack alignItems="center" justifyContent="space-between">
-                <Text fontWeight="medium">Location</Text>
-              </HStack>
-              <HStack alignItems="center" justifyContent="space-between">
-                <Text color="blueGray.400">Select 1 or more</Text>
-              </HStack>
-              <HStack alignItems="center" justifyContent="space-between">
-              <Checkbox.Group onChange={setLocationValues} value={locationValues} accessibilityLabel="choose numbers">
-              <Checkbox value="one" my={2}>
-                East
-              </Checkbox>
-                <Checkbox value="two">West</Checkbox>
-              </Checkbox.Group>
-              </HStack>
-              <HStack alignItems="center" justifyContent="space-between">
-                <Text fontWeight="medium">Reviews</Text>
-              </HStack>
-              <HStack alignItems="center" justifyContent="space-between">
-                <Text color="blueGray.400">Select the minimum stars (if you select 3,
-                you'll get 3 and up)</Text>
-              
-              </HStack>
-
-              <HStack alignItems="center" justifyContent="space-between">
-              <AirbnbRating />
-
+          <VStack space={4}>
                 
+                
+                <VStack space = {1}>
+                    <VStack space = {0}>
+                        <HStack alignItems="center">
+                            <Icon as={MaterialCommunityIcons} name="map-marker-outline" color = "grey"/>
+                            <Text fontWeight="medium">Location</Text>
+                        </HStack>
+                        <HStack alignItems="center" justifyContent="space-between">
+                            <Text color="blueGray.400">         Select 1 or more</Text>
+                        </HStack>
+                    </VStack>
+                        <HStack alignItems="center" justifyContent="space-between" marginLeft={30}>
+                            <Checkbox.Group onChange={setLocationValues} value={locationValues} accessibilityLabel="choose numbers">
+                            <Checkbox value="one" my={2}>
+                                East
+                            </Checkbox>
+                                <Checkbox value="two">West</Checkbox>
+                            </Checkbox.Group>
+                        </HStack>
+        
+                </VStack>
+              
+              
+              
+              
+                <HStack alignItems="center" >
+                    <Icon as={MaterialCommunityIcons} name="star-outline" color = "grey" size = {8}/>
+                    <Text fontWeight="medium">Reviews</Text>
+                </HStack>
 
-              </HStack>
-              <HStack alignItems="center" justifyContent="space-between">
-                <Text fontWeight="medium">Beds</Text>
-              </HStack>
-              <HStack alignItems="center" justifyContent="space-between">
-                <Text color="blueGray.400">Select 1 or more</Text>
-              </HStack>
-              <HStack alignItems="center" justifyContent="space-between">
-              <Checkbox.Group onChange={setRoommateValues} value={roommateValues} accessibilityLabel="choose numbers">
-              <Checkbox value="one" my={2}>
-                1
-              </Checkbox>
-                <Checkbox value="two">2</Checkbox>
-                <Checkbox value="one" my={2}>
-                3
-              </Checkbox>
-                <Checkbox value="two">4+</Checkbox>
-              </Checkbox.Group>
-              
-              </HStack>
-              <HStack alignItems="center" justifyContent="space-between">
-                <Text fontWeight="medium">Bathroom</Text>
-              </HStack>
-              <HStack alignItems="center" justifyContent="space-between">
-                <Text color="blueGray.400">Select 1 or more</Text>
-              </HStack>
-              <HStack alignItems="center" justifyContent="space-between">
-              <Checkbox.Group onChange={setBathroomValues} value={bathroomValues} accessibilityLabel="choose numbers">
-              <Checkbox value="one" my={2}>
-                Single
-              </Checkbox>
-                <Checkbox value="two">Shared - 1 other person</Checkbox>
-                <Checkbox value="one" my={2}>
-                Shared floor - same gender
-              </Checkbox>
-              </Checkbox.Group>
-              
-              </HStack>
-              <HStack alignItems="center" justifyContent="space-between">
-                <Text fontWeight="medium">Budget</Text>
-              </HStack>
-              <HStack alignItems="center" justifyContent="space-between">
-                <Text color="blueGray.400">Use slider (price per semester)</Text>
-              </HStack>
-              <HStack alignItems="center" justifyContent="space-between">
+                <VStack space = {0}>
+                    <HStack alignItems="center" justifyContent="space-between">
+                    <HStack>
+                    <AirbnbRating />
+                    <Text color="blueGray.400">& up</Text>
+                    </HStack>
+                    </HStack>
+                </VStack>
 
-              <MultiSlider
-              values={[0, 5000]}
-              isMarkersSeparated={true}
-              min={0}
-              max={5000}
-              onValuesChange={(values) => setBudgetValues(values)}
-              step={100}
-              allowOverlap={false}
-              minMarkerOverlapDistance={10}
-                />
-                </HStack>
-                <HStack>
-                <Text bold>Min: {budgetValues[0]}</Text>
-                </HStack>
-                <HStack>
-                <Text bold>Max: {budgetValues[1]}</Text>
-                </HStack>
-              
+                <VStack space = {1}>
+                    <VStack space = {0}>
+                        <HStack alignItems="center">
+                            <Icon as={MaterialCommunityIcons} name="bed-single-outline" color = "grey"/>
+                            <Text fontWeight="medium">Beds</Text>
+                        </HStack>
+                        <HStack alignItems="center" justifyContent="space-between">
+                            <Text color="blueGray.400">         Select 1 or more</Text>
+                        </HStack>
+                    </VStack>
+                    
+                    <HStack alignItems="center" marginLeft={30}>
+
+                    <Checkbox.Group onChange={setRoommateValues} value={roommateValues} accessibilityLabel="choose numbers">
+                        <Checkbox value="one" >
+                            1
+                        </Checkbox>
+                            <Checkbox value="two">2</Checkbox>
+                            <Checkbox value="one"> 3 </Checkbox>
+                            <Checkbox value="two">4+</Checkbox>
+                        </Checkbox.Group>
+                    
+                    </HStack>
+                </VStack>
+
+
+                <VStack space = {1}>
+                    <VStack space = {0}>
+                        <HStack alignItems="center">
+                            <Icon as={MaterialCommunityIcons} name="shower" color = "grey" size ={6}/>
+                            <Text fontWeight="medium"> Bathroom</Text>
+                        </HStack>
+                        <HStack alignItems="center" justifyContent="space-between">
+                            <Text color="blueGray.400">Select 1 or more</Text>
+                        </HStack>
+                        <HStack alignItems="center" justifyContent="space-between" paddingLeft={30}>
+                        <Checkbox.Group onChange={setBathroomValues} value={bathroomValues} accessibilityLabel="choose numbers">
+                        <Checkbox value="one" my={2}>
+                            Single
+                        </Checkbox>
+                            <Checkbox value="two">Shared - 1 other person</Checkbox>
+                            <Checkbox value="one" my={2}>
+                            Shared floor - same gender
+                        </Checkbox>
+                        </Checkbox.Group>
+                        </HStack>
+                    </VStack>
+                </VStack>
+
+                <VStack space = {1}>
+                    <VStack space = {0}>
+                        <HStack alignItems="center" justifyContent="space-between">
+                            <Text fontWeight="medium">Budget</Text>
+                        </HStack>
+                        <HStack alignItems="center" justifyContent="space-between">
+                            <Text color="blueGray.400">Use slider (price per semester)</Text>
+                        </HStack>
+                        <HStack  width={1}>
+
+                            <MultiSlider
+                            values={[0, 5000]}
+                            isMarkersSeparated={true}
+                            min={0}
+                            max={5000}
+                            onValuesChange={(values) => setBudgetValues(values)}
+                            step={100}
+                            allowOverlap={false}
+                            minMarkerOverlapDistance={10}
+                                />
+                        </HStack>
+                            <HStack justifyContent="space-between">
+                            <Text bold>Min: {budgetValues[0]}</Text>
+                            <Text bold>Max: {budgetValues[1]}</Text>
+                            </HStack>
+                
+                        
+                        </VStack>
+                </VStack>
             </VStack>
           </Modal.Body>
           <Modal.Footer>
