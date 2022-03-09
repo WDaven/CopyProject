7f2f190ff1566b93585d1ad0b8a0729593ed0675
WDaven <71561085+WDaven@users.noreply.github.com>
2022-03-09 11:23:56 -0500

    add one marker, has onpress print

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { Marker } from 'react-native-maps'; 
+import PreviewPressableTile from './PreviewPressableTile';
 
 export default function MapComponent() {
   return (
@@ -11,7 +13,18 @@ export default function MapComponent() {
         longitude: -84.398471,
         latitudeDelta: 0.0422,
         longitudeDelta: 0.019,
-      }}/>
+      }}>
+        <Marker
+            key={'index'}
+            coordinate={{
+                latitude: 33.77843874968607,
+                longitude: -84.4038864219189,
+              }}
+              onPress={() => console.log("I'm Pressed")}
+            title={'Fitten'}
+            description={'test'}
+        />
+          </MapView>
     </View>
   );
 }
